e5c1499c79695e691f9f602844c06ed0049d0eb0
rousell <lauren.l.rouse@gmail.com>
2016-05-31 19:00:44 -0500

    added initial models, added repo and context

---
+++ gentrification-calc/DAL/CalcRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gentrification_calc.DAL
+{
+    public class CalcRepository
+    {
+    }
+}
---
+++ gentrification-calc/Models/DemographicInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gentrification_calc.Models
+{
+    public class Demographics
+    {
+        public int ZipCode { get; set; }
+        public int DemographicsId { get; set; }
+        public int DemographicsYear { get; set; }
+        public int Population { get; set; }
+        public string Race { get; set; }
+    }
+}

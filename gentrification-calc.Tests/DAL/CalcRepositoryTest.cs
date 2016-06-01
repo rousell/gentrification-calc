36eaccc37de30779ac0fac54718a25bb2794dbdd
rousell <lauren.l.rouse@gmail.com>
2016-06-01 17:02:36 -0500

    Initial Repository Test Created

---
+++ gentrification-calc.Tests/DAL/CalcRepositoryTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using gentrification_calc.DAL;
+
+namespace gentrification_calc.Tests.DAL
+{
+    [TestClass]
+    public class CalcRepositoryTest
+    {
+        [TestMethod]
+        public void RepoEnsureICanCreateAnInstance()
+        {
+            CalcRepository repo = new CalcRepository();
+            Assert.IsNotNull(repo);
+        }
+    }
+}

4572a92152c51eaf8f9f3bd6c67f621637bd3df4
rousell <lauren.l.rouse@gmail.com>
2016-06-08 18:03:59 -0500

    added correct console application

--- CsvConsoleReader/Program.cs
+++ CsvConsoleReader/Program.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
-namespace CsvConsole
+namespace CsvConsoleReader
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
         }
     }

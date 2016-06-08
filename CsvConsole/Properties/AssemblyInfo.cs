cb8e6f66f70013c2a70a1ba5d7fad89636a4f05e
rousell <lauren.l.rouse@gmail.com>
2016-06-08 17:56:20 -0500

    changed console part again

--- CsvConsole/Properties/AssemblyInfo.cs
+++ CsvConsole/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CsvConsoleApp")]
+[assembly: AssemblyTitle("CsvConsole")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CsvConsoleApp")]
+[assembly: AssemblyProduct("CsvConsole")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b388c3f9-09e5-49dc-a9ca-888a00be39e2")]
+[assembly: Guid("d57b5eb2-bd5e-4392-a93d-abe2dff6d6b6")]

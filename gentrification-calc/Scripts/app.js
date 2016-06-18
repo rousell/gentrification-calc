9ca39a9bdd974f469a4e9ee0cdfa5c6e90ce6818
rousell <lauren.l.rouse@gmail.com>
2016-06-18 17:41:41 -0500

    added test for map instance

--- gentrification-calc/Scripts/app.js
+++ gentrification-calc/Scripts/app.js
@@ -2,6 +2,14 @@
 
     'use strict';
 
-    var app = angular.module("GentrificationCalcApp", []);
+    angular
+        .module("GentrificationCalcApp", [])
+        .controller('MapCtrl', MapCtrl);
+
+    function MapCtrl (){
+        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+    }
+
 
 })();

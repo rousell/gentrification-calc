c53fd3a2556e9c7f66fef52c894dae29fd859837
rousell <lauren.l.rouse@gmail.com>
2016-06-19 08:32:24 -0500

    added mapbox tiles as layer

--- gentrification-calc/Scripts/app.js
+++ gentrification-calc/Scripts/app.js
@@ -30,17 +30,19 @@ angular.module('app').controller('rootController', [
             '$scope', 'leaflet',
     function ($scope, leaflet) {
         leaflet.map.then(function (map) {
-            L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-                maxZoom: 18
+            L.tileLayer('http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                maxZoom: 18,
+                id: 'mapbox.streets',
+                accessToken: 'pk.eyJ1IjoibHJvdXNlIiwiYSI6ImNpaHBnYmkxaDA0NGJ0c20yMG5sMmZlenIifQ.NVpXXlzfBCtK00m36zp68Q'
             }).addTo(map);
             map.setView([36.1666, -86.7833], 12);
             L.marker([36.1666, -86.7833]).addTo(map);
-            L.polygon([
-                [36.18, -86,78],
-                [36.19, -86,77],
-                [36.12, -86,8]
-            ]).addTo(map);    
+            L.circle([36.1666, -86.7833],{
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5
+            }).addTo(map);    
         });
     }
 ]);

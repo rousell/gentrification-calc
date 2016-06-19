6d7b76cc9ad7383dca218eed3ac96b639cfb2976
rousell <lauren.l.rouse@gmail.com>
2016-06-19 08:33:31 -0500

    fixed api mapbox tile uri

--- gentrification-calc/Scripts/app.js
+++ gentrification-calc/Scripts/app.js
@@ -30,7 +30,7 @@ angular.module('app').controller('rootController', [
             '$scope', 'leaflet',
     function ($scope, leaflet) {
         leaflet.map.then(function (map) {
-            L.tileLayer('http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            L.tileLayer('http://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
                 attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                 maxZoom: 18,
                 id: 'mapbox.streets',
